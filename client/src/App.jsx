f241a06d4e294a9f2defcf6fce59fbd330139af9
ankit4vision <ankit4vision@gmail.com>
2025-10-06 15:06:30 +0530

    added hasrouter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { LayoutWrapper, ScrollToTop } from './components';
 import { CartProvider, UserProvider } from './context';
 import { navItemsData, footerLinksData, socialLinksData } from './data/mockData';
